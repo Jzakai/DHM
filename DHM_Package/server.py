0ea13079a9ff33f3696aa69bec7ce9a6b7686700
Jzakai <Jana.zakai@gmail.com>
2025-07-31 16:46:14 +0300

    edited server

--- DHM_Package/server.py
+++ DHM_Package/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, Form
+from fastapi import FastAPI, UploadFile
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import numpy as np
